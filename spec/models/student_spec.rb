08f79ad8225de346fe67be6b2f5497c6e56e2e25
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-12-31 19:25:19 -0700

    Model test for user story 4

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
+  let!(:malfoy){Student.create!(name: "Malfoy Draco", age: 12, house: "Slytherin")}
+  let!(:harry){Student.create!(name: "Harry Potter", age: 12, house: "Gryffindor")}
+  let!(:cedric){Student.create!(name: "Cedric Diggory", age: 15, house: "Hufflepuff")}
 
   describe 'validations' do
     it {should validate_presence_of :name}
@@ -11,6 +14,12 @@ RSpec.describe Student, type: :model do
   describe 'relationships' do
     it {should have_many :student_courses}
     it {should have_many(:courses).through(:student_courses)}
+  end
+
+  describe 'class methods' do
+    it '#average_age' do
+      expect(Student.average_age).to eq 13
+    end
   end
 
 end
